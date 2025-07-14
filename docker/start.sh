9cab3efe4eb5f3b1dd4950f69ac59e57c3f813ba
Uziel_Lujan <uzielisai77@gmail.com>
2025-07-14 17:51:45 -0600

    fix: Usa la ruta de configuración explícita en el script de inicio

--- docker/start.sh
+++ docker/start.sh
@@ -4,4 +4,5 @@ chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Inicia Supervisor para correr Nginx y PHP-FPM
-exec /usr/bin/supervisord -c /etc/supervisor/supervisor.conf
+# Se corrige el nombre del archivo de configuracion
+exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
