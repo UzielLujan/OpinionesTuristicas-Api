2d516907b70bcd831b9889faa303fd00b5403392
Uziel_Lujan <uzielisai77@gmail.com>
2025-07-14 18:25:21 -0600

    fix: Reconfigura Nginx, PHP y Supervisor para despliegue

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-# Asigna permisos correctos al storage
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
-
 # Inicia Supervisor para correr Nginx y PHP-FPM
-# Se corrige el nombre del archivo de configuracion
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
