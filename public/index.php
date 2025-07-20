d4cf3ec1a83aa21b68395c52e16fa6cbb79be03e
Uziel_Lujan <uzielisai77@gmail.com>
2025-07-19 18:15:05 -0600

    fix(composer.json): Clear automatic discovery for jenssegers/laravel-mongodb package
    fix(index.php): Add debug logging for application bootstrap process

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+error_log('DEBUG: index.php - Start');
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
@@ -10,11 +12,16 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+error_log('DEBUG: index.php - Before Autoload');
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+error_log('DEBUG: index.php - Before App Bootstrap');
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+error_log('DEBUG: index.php - Before Request Handle');
 $app->handleRequest(Request::capture());
+
+error_log('DEBUG: index.php - End');
